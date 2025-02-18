d9539b740dcf35830609dc599e90aff8710587ca
elvis-chavez <elvis.chavez@catolica.edu.sv>
2025-02-18 14:25:07 -0600

    Rodrigo ha Implenetado las subpaginas de 'Donativos' y 'Quienes somos'

---
+++ ONG ANIMALES V4/ONG ANIMALES V4/Pages/QuienesSomos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ONG_ANIMALES_V4.Pages
+{
+    public class QuienesSomosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
