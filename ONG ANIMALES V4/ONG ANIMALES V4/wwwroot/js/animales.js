0d6bc28d7ece44256b2f5febce55e1164ec1a39c
elvis-chavez <elvis.chavez@catolica.edu.sv>
2025-02-27 18:06:35 -0600

    Rodrigo a implementado la subpagina de "Animales",con su respectivo js(animales.js)

---
+++ ONG ANIMALES V4/ONG ANIMALES V4/wwwroot/js/animales.js
@@ -0,0 +1,38 @@
+﻿let animalCurrentIndex = 0;
+const animalCarouselContainer = document.getElementById('animalCarouselContainer');
+const animalImages = document.querySelectorAll('.animal-carousel img');
+
+function nextAnimalSlide() {
+    animalCurrentIndex = (animalCurrentIndex + 1) % animalImages.length;
+    updateAnimalCarousel();
+}
+
+function prevAnimalSlide() {
+    animalCurrentIndex = (animalCurrentIndex - 1 + animalImages.length) % animalImages.length;
+    updateAnimalCarousel();
+}
+
+function updateAnimalCarousel() {
+    const offset = -animalCurrentIndex * 310;
+    animalCarouselContainer.style.transform = `translateX(${offset}px)`;
+}
+
+function toggleAnimalInfo() {
+    const infoDiv = document.getElementById('animalInfo');
+    const activeImage = animalImages[animalCurrentIndex];
+    infoDiv.textContent = activeImage.getAttribute('data-info');
+    infoDiv.style.display = infoDiv.style.display === 'block' ? 'none' : 'block';
+}
+
+//function playAnimalSound() {
+//    const activeImage = animalImages[animalCurrentIndex];
+//    const sound = new Audio(activeImage.getAttribute('data-sound'));
+//    sound.play();
+//}
+
+function playSound(animal) {
+    let sonido = document.getElementById(animal);
+    sonido.play();
+}
+
+let isPaused = false; // Variable global para controlar la pausa
